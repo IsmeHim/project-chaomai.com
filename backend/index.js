c2ea4afb3e0462d5a9eb8958ad2c96fca54d4771
Him <abdulrahimhim2@gmail.com>
2025-08-20 14:39:53 +0700

    ใช้ routes แบบวนลูป

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const { readdirSync } = require('fs') //import อันนี้ไดเลยเพื่อวนลูปทุกเราท์
 
 const app = express();
 
 // Routes
-const authRoutes = require('./router/auth');
-const protectedRoutes = require('./router/protected');
-const taxonomyRouter = require('./router/taxonomy');
+// const authRoutes = require('./router/auth');
+// const protectedRoutes = require('./Routes/protected');
+// const taxonomyRouter = require('./Routes/taxonomy');
+// Route 2
+readdirSync('./router')
+    .map((r) => app.use('/api', require('./router/' + r)))
+
 
 // Middleware
 app.use(cors({
@@ -23,9 +28,11 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("✅ MongoDB connected"))
     .catch(err => console.error("❌ MongoDB connection error:", err));
 
-app.use('/api/auth', authRoutes);
-app.use('/api', protectedRoutes);
-app.use('/api', taxonomyRouter);
+
+// ใช้งานเราท์ที่ import มา
+// app.use('/api/auth', authRoutes);
+// app.use('/api', protectedRoutes);
+// app.use('/api', taxonomyRouter);
 
 const PORT = process.env.PORT || 5000;
 
